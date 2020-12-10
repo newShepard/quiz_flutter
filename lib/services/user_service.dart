9d79013607433e421cc007faf900d582555c28a5
sergey.voinkov <pancake@mbp.local>
2020-12-10 23:26:02 +0200

    user service inprogress

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -11,4 +11,24 @@ class UserService {
   UserService({required this.mdtApiClient, required this.mdtAuthModule}) {
     this.privacyPolicyAccepted = true;
   }
+
+  // add events
+
+  void log() {}
+
+  void savePrivacyPolicyAcceptLocaly() {}
+
+  bool get privacyPolicyIsAccepted => this.privacyPolicyAccepted;
+
+  Future<void> init() async {}
+
+  Future<void> checkPrivacyPolicy() async {}
+
+  Future<void> savePrivacyPolicyAcceptToServer() async {}
+
+  Future<void> prepareLogData() async {}
+
+  Future<void> changeLanguage({required int langId}) async {}
+
+  Future<void> updateUserSettings(QuizUser? user) async {}
 }
