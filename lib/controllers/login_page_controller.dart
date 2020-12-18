031ca2a4bbf8f6c67161e445ef0c3f62c3b98dfd
sergey.voinkov <pancake@mbp.local>
2020-12-18 23:06:34 +0200

    changed: routes navigation

--- lib/controllers/login_page_controller.dart
+++ lib/controllers/login_page_controller.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quiz_flutter/helpers/dependecy_injector.dart';
+import 'package:quiz_flutter/services/auth_service.dart';
 
 class LoginPageController extends GetxController {
-  final email = TextEditingController().obs;
-  final password = TextEditingController().obs;
-  final ready = false.obs;
-  final error = dynamic.obs;
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  dynamic error;
+
+  AuthService _authService;
+
+  @override
+  void onInit() {
+    this._authService = sl<AuthService>();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    email.dispose();
+    password.dispose();
+    super.onClose();
+  }
 
   void changeEmail(String value) {
-    this.email.value.text = value;
+    this.email.text = value;
   }
 
   void clearEmail() {
-    this.email.value.text = "";
+    this.email.text = "";
   }
 
   void changePassword(String value) {
-    this.password.value.text = value;
+    this.password.text = value;
   }
 
   void clearPassword() {
-    this.password.value.text = "";
+    this.password.text = "";
   }
 
-  Future<void> onSignIn() async {
-    print("SignIn");
+  Future<void> singIn() async {
+    await this
+        ._authService
+        .signIn(
+            login: this.email.value.text, password: this.password.value.text)
+        .then((value) => null)
+        .catchError((error) {
+      this.error = error;
+      update();
+    });
   }
 }
