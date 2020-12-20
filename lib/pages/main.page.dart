d3b5848110e5cfdb6889dd2dfeec6f5943ab80e0
sergey.voinkov <meltagun1@gmail.com>
2020-12-20 23:13:06 +0200

    changed: main page icons

--- lib/pages/main.page.dart
+++ lib/pages/main.page.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Ближайшие'),
-        //automaticallyImplyLeading: false, // backlink
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedItemColor: Colors.blue,
-        items: [
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/main_page_tab_bar/visits_history.svg',
-                  color: Colors.black),
-              backgroundColor: Colors.red,
-              label: 'visits'),
-          BottomNavigationBarItem(
-              icon:
-                  SvgPicture.asset('assets/icons/main_page_tab_bar/outlet.svg'),
-              label: 'routes'),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/main_page_tab_bar/profile.svg'),
-              label: 'profile'),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Ближайшие'),
+          //automaticallyImplyLeading: false, // backlink
+        ),
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Center(
+            child: Text("Hello"),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.format_list_bulleted,
+                  size: 48,
+                ),
+                label: 'visits'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.storefront_outlined,
+                  size: 48,
+                ),
+                label: 'routes'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline,
+                  size: 48,
+                ),
+                label: 'profile'),
+          ],
+        ),
       ),
     );
   }
