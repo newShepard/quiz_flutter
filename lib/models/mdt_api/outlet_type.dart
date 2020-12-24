ff0f748c7d60876ba2fa551d29e4b7b1bac42c52
sergey.voinkov <meltagun1@gmail.com>
2020-12-24 23:15:57 +0200

    added: mdt api outlet type model

---
+++ lib/models/mdt_api/outlet_type.dart
@@ -0,0 +1,16 @@
+import 'package:quiz_flutter/models/common.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'outlet_type.g.dart';
+
+@JsonSerializable(createToJson: false)
+class MdtApiOutletType with Recordable, Nameable {
+  String code;
+  String icon;
+  String uid_ds;
+
+  MdtApiOutletType();
+
+  factory MdtApiOutletType.fromJson(Map<String, dynamic> json) =>
+      _$MdtApiOutletTypeFromJson(json);
+}
---
+++ lib/models/mdt_api/outlet_type.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'outlet_type.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MdtApiOutletType _$MdtApiOutletTypeFromJson(Map<String, dynamic> json) {
+  return MdtApiOutletType()
+    ..id = json['id'] as int
+    ..name = json['name'] as String
+    ..code = json['code'] as String
+    ..icon = json['icon'] as String
+    ..uid_ds = json['uid_ds'] as String;
+}
