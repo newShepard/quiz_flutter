4a21748cce0df528b180b62003bdec1276982cbe
sergey.voinkov <meltagun1@gmail.com>
2020-12-30 01:01:23 +0200

    inprogress

--- lib/tabs/outlets_tab.dart
+++ lib/tabs/outlets_tab.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_flutter/helpers/dependecy_injector.dart';
-import 'package:quiz_flutter/models/mdt_api/outlet.dart';
 import 'package:quiz_flutter/services/outlet_service.dart';
 
 class OutletsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var outletService = sl<OutletService>();
-    return const Center(child: Text('Qwerty'));
+    return FutureBuilder(
+      future: outletService.getNearestOutlets(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (BuildContext context, int idx) {
+              var outlet = snapshot.data[idx].outlet;
+              return ListTile(
+                isThreeLine: true,
+                title: Text(outlet.code),
+                subtitle: Text(outlet.address),
+                trailing: const Icon(Icons.keyboard_arrow_right_outlined),
+                //trailing: ,
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
-
-// FutureBuilder(
-//         future: outletService.getNearestOutlets(),
-//         builder: (BuildContext context, AsyncSnapshot snapshot) {
-//           if (snapshot.hasData) {
-//             return ListView.builder(
-//                 itemCount: snapshot.data?.records?.length,
-//                 itemBuilder: (BuildContext context, int idx) {
-//                   var outlet = MdtApiOutlet.fromJson(
-//                       snapshot.data?.records[idx]['id_Outlet\$']);
-//                   return ListTile(
-//                     isThreeLine: true,
-//                     title: Text(outlet.code),
-//                     subtitle: Text(outlet.address),
-//                     trailing: const Icon(Icons.keyboard_arrow_right_outlined),
-//                     //trailing: ,
-//                   );
-//                 });
-//           }
-//         },
-//       ),
